c80d460ed2a411c02924e475e2f629349fa48dba
cs50fran <francisco.pedrasribeiro@gmail.com>
2026-02-11 19:18:40 +0000

    ex1

---
+++ ft_garden_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+class Plant:
+	def __init__(self, name: str, height: int, age: int) -> None:
+		self.name: str = name
+		self.height: int = height
+		self.age: int = age
+
+def ft_garden_data() -> list:
+	return [
+		Plant("Rose", 25, 30),
+		Plant("Sunflower", 80, 45),
+		Plant("Cactus", 15, 120)
+	]
+
+def main():
+	plants = ft_garden_data()
+	for plant in plants:
+		print(f"{plant.name}: {plant.height}cm, {plant.age} days old")
+
+if __name__ == "__main__":
+	main()
