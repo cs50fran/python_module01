282518a6e1adebbf35bf0528cb2a50e7121f1312
cs50fran <francisco.pedrasribeiro@gmail.com>
2026-02-11 18:55:20 +0000

    ex00

---
+++ ft_garden_intro.py
@@ -0,0 +1,16 @@
+def ft_garden_intro(plant, height, age):
+	print("=== Welcome to My Garden ===")
+	print(f"Plant: {plant}")
+	print(f"Height: {height}")
+	print(f"Age: {age}\n")
+	print("=== End of Program ===")
+
+def main():
+	
+	plant: str = "Morango"
+	height: str = "50cm"
+	age: str=  "30 days"
+	ft_garden_intro(plant, height, name)
+
+if __name__ == "__main__":
+	main()
